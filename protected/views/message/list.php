fcfd4b887114863d58d65ebe50b5ae0728eeefe0
李均洽 <493546053@qq.com>
2017-08-21 12:42:46 +0800

    优化

--- protected/views/message/list.php
+++ protected/views/message/list.php
@@ -176,7 +176,7 @@ $this->pageTitle = '聊天信息';
 
             var sw = false;
             if(result.code == 0 ) {
-                maxchatid = result.data.id
+                maxchatid = result.items.id;
                 if(typeof (window.external.showServiceWindow) == 'function')
                     window.external.showServiceWindow();
             }
