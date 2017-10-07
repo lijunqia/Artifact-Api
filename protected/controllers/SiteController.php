89140d338e61ce8c49dbb7e84dca94094e9ec80d
李均洽 <493546053@qq.com>
2017-10-07 10:28:27 +0800

    用户注册

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -105,6 +105,50 @@ class SiteController extends Controller
 	}
 
 	/**
+	 * 注册接口
+	 * @appkey 系统分配应用KEY
+	 * @pid 邀请人ID
+	 * @pcode 邀请人账户或手机
+	 * @code 账号,可为空
+	 * @pwd 密码,必填
+	 * @name 姓名,可为空
+	 * @mobie 手机,必填
+	 * @email 邮箱,可为空
+	 * @province 省
+	 * @city 市
+	 * @area 区
+	 * @key 验证验证码成功后返回的参数再回传到服务器
+	 */
+	public function actionRegister()
+	{
+		$params = array(
+			'pcode' => Yii::app()->request->getParam('pcode',''),
+			'service' => intval(Yii::app()->request->getParam('service',0)),
+			'code' => Yii::app()->request->getParam('code',''),
+			'name' => Yii::app()->request->getParam('name',''),
+			'pwd' => Yii::app()->request->getParam('pwd',''),
+			'mobile' => Yii::app()->request->getParam('mobile',''),
+			'email' => Yii::app()->request->getParam('email',''),
+			'remark' => Yii::app()->request->getParam('remark',''),
+			'role' => 5,
+			'expire' => Yii::app()->request->getParam('expire',date('Y-m-d H:i:s',strtotime("+1 month"))),
+		);
+
+		if(empty($params['code']))
+			$this->response(1006);
+
+		$model = User::model()->regist($params);
+		if(is_object($model))
+		{
+			$this->response(0,$model->attr());
+		}
+		if(is_int($model))
+			$this->response($model);
+		else
+			$this->response(1001);
+	}
+
+	/**
 	 * This is the default 'index' action that is invoked
 	 * when an action is not explicitly requested by users.
 	 */
